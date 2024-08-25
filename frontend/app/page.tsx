10ad1058847164408cd3c519424bd14600fae6b2
Javier Martinez <hoogmin@gmail.com>
2024-08-25 16:07:58 -0400

    Initialize Redux.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+"use client"
+
+import { useAppSelector, useAppDispatch } from "@/lib/hooks"
+import { increment, decrement } from "@/lib/features/counter/counterSlice";
 
 export default function Home() {
+  const count = useAppSelector(state => state.counter.value)
+  const dispatch = useAppDispatch()
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>This is for the app.</p>
+      <div className="flex flex-row space-x-5">
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <p>Count: {count}</p>
+      </div>
     </main>
   );
 }
